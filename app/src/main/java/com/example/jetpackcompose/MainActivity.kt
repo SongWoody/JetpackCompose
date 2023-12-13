22e72dbd7c87f414068ea912282ec7e46c90f551
Woody <syg1138@gmail.com>
2023-12-13 22:34:37 +0900

    Init JetpackCompose Project

---
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                DemoScreen()
+            }
+        }
+    }
+}
+
+/**
+ * Demo Text
+ */
+@Composable
+fun DemoText(message: String, fontSize: Float) {
+    Text(
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Preview
+@Composable
+fun DemoTextPreView() {
+    DemoText(message = "Welcome to Compose", fontSize = 16f)
+}
+
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float)->Unit) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..40f,
+        value = sliderPosition, onValueChange = onPositionChange)
+}
+
+@Composable
+fun DemoScreen() {
+    var sliderPosition by remember { mutableStateOf(20f) }
+
+    val handlePositionChange = { position: Float ->
+        sliderPosition = position
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        DemoText(message = "Welcome to Compose", fontSize = sliderPosition)
+
+        Spacer(modifier = Modifier.height(150.dp))
+
+        DemoSlider(sliderPosition = sliderPosition, onPositionChange = handlePositionChange)
+
+        Text(
+            style = MaterialTheme.typography.headlineMedium,
+            text = "${sliderPosition.toInt()} sp"
+        )
+    }
+}
