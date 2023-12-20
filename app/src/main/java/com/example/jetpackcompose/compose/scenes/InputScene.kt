b2b45db5f3ec6fb995301470e7426d17033c4a9b
Woody <syg1138@gmail.com>
2023-12-20 19:46:26 +0900

    InputScene 디자인 변경

--- app/src/main/java/com/example/jetpackcompose/compose/scenes/InputScene.kt
+++ app/src/main/java/com/example/jetpackcompose/compose/scenes/InputScene.kt
@@ -1,13 +1,21 @@
 package com.example.jetpackcompose.compose.scenes
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -15,6 +23,16 @@ fun InputScene(modifier: Modifier, text: String = "", onValueChange: (String)->U
     Scaffold(topBar = {
         TopAppBar(title = { Text(text = "InputScene") })
     }) { paddingValues ->
-        TextField(modifier = modifier.padding(paddingValues), value = text, onValueChange = onValueChange)
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment =  Alignment.CenterHorizontally
+        ) {
+            TextField(modifier = modifier.padding(paddingValues), value = text, onValueChange = onValueChange)
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(onClick = { }) {
+                Text(text = "Submit")
+            }
+        }
     }
 }
